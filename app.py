a3856ab27e9a3dce0c653a74a9ab1e3b5f87609c
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-09-11 09:14:46 -0300

    :sparkles: feat: Atualização no app.py

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ import yt_dlp as ytdlp
 
 
 def download_audio(video_url: str, audio_path: str) -> str:
-    output_dir = './audios'
+    output_dir = "./audios"
     os.makedirs(output_dir, exist_ok=True)
 
     full_audio_path = os.path.join(output_dir, f"{audio_path}.mp4")
 
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': full_audio_path,
-        'noplaylist': True,
+        "format": "bestaudio/best",
+        "outtmpl": full_audio_path,
+        "noplaylist": True,
     }
 
     try:
@@ -27,32 +27,36 @@ def download_audio(video_url: str, audio_path: str) -> str:
 
 
 def transcribe_audio(audio_path: str) -> str:
-    model = whisper.load_model('base')
+    model = whisper.load_model("base")
     result = model.transcribe(audio_path)
-    return result['text']
+    return result["text"]
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download audio and transcribe from YouTube')
+    parser = argparse.ArgumentParser(
+        description="Download audio and transcribe from YouTube"
+    )
 
-    parser.add_argument('--url', type=str, required=True, help='YouTube video URL')
-    parser.add_argument('--path', type=str, default='audio', help='Output file name (without extension)')
+    parser.add_argument("--url", type=str, required=True, help="YouTube video URL")
+    parser.add_argument(
+        "--path", type=str, default="audio", help="Output file name (without extension)"
+    )
 
     args = parser.parse_args()
 
     try:
         full_audio_path = download_audio(args.url, args.path)
+
+        try:
+            transcription = transcribe_audio(full_audio_path)
+            logger.success("Transcription success!")
+            print("Transcription:\n", transcription)
+        except Exception as e:
+            logger.error(f"Error during transcription: {e}")
+
     except Exception as e:
         logger.error(f"Error during audio download: {e}")
-        return
-
-    try:
-        transcription = transcribe_audio(full_audio_path)
-        logger.success('Transcription success!')
-        print("Transcription:\n", transcription)
-    except Exception as e:
-        logger.error(f"Error during transcription: {e}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
